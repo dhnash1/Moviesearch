ad1180c91bb1d066080477d877ff031595b0ecfa
Deforis Nash <defhnash@gmail.com>
2016-11-30 20:05:06 -0800

    Adds title, image, and date to DOM

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,32 @@ $(document).ready(function(){
 
 $(document).on('click', '#button', function(){
   console.log('ya clicked it');
+  if (!$('#searchBox').val() ) {
+    alert("Input something!");
+  }else{
   var input = $('#searchBox').val();
   var website = "http://www.omdbapi.com/?s=" + input;
   $.ajax({
     url:website,
     dataType:'JSON',
     success: function(data){
-      console.log('found', data);
-      thing1(data);
+      console.log('found', data.Search);
+      thing1(data.Search);
+
       }//end success
     });//end ajax
+}
+var thing1 = function(rango){
+  console.log("Hey!", rango);
+  $('#movieDiv').empty();
+  for (var i = 0; i < rango.length; i++) {
 
-var thing1 = function(x){
-  console.log("Hey!", x);
 
+    $('#movieDiv').append('<h1>' + rango[i].Title + '</h1>');
+    $('#movieDiv').append("<img src = '" + rango[i].Poster + "'/>");
+    $('#movieDiv').append("<p class='year'>" + rango[i].Year + "</p>");
 
+}
 
 };//endthing1
 
