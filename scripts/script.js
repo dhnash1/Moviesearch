d33dab6a5fb7f53dbbe805f7634e8153f8710aef
Deforis Nash <defhnash@gmail.com>
2016-12-01 08:55:19 -0600

    added not found message

--- scripts/script.js
+++ scripts/script.js
@@ -20,16 +20,21 @@ $(document).on('click', '#button', function(){
 var thing1 = function(rango){
   console.log("Hey!", rango);
   $('#movieDiv').empty();
-  for (var i = 0; i < rango.length; i++) {
-      $('#movieDiv').append("<div id='inDiv" + [i] + "' class='movie'></div>");
-      $('#inDiv' + [i]).append('<h1>' + rango[i].Title + '</h1>');
-      $('#inDiv' + [i]).append("<p class='year'>" + rango[i].Year + "</p>");
-      if( rango[ i ].Poster == "N/A"){
-        $( '#inDiv' + [i] ).append("<p>No Image found!</p>");
-      }else{
-      $('#inDiv' + [i]).append("<img src = '" + rango[i].Poster + "'/>");
-        }//end Else
-      }//end forloop
+  if(rango == undefined){
+    console.log("Nope!");
+    $('#movieDiv').append("<h1>No movies found with that title!<h1>");
+  }else{
+    for (var i = 0; i < rango.length; i++) {
+        $('#movieDiv').append("<div id='inDiv" + [i] + "' class='movie'></div>");
+        $('#inDiv' + [i]).append('<h1>' + rango[i].Title + '</h1>');
+        $('#inDiv' + [i]).append("<p class='year'>" + rango[i].Year + "</p>");
+        if( rango[ i ].Poster == "N/A"){
+          $( '#inDiv' + [i] ).append("<p>No Image found!</p>");
+        }else{
+        $('#inDiv' + [i]).append("<img src = '" + rango[i].Poster + "'/>");
+          }//end Else
+        }//end forloop
+      }
     };//endthing1
   });//end on click
     function easteregg(){
