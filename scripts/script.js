4fff9b04f946e4b0bfd193c7065b1c5bd189d742
Deforis Nash <Deforis Nash>
2016-11-30 19:07:31 -0800

    added AJAX

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,25 @@
-console.log("jeveh");
 $(document).ready(function(){
-  console.log("jehqueeree");
+    var printo = function(dayta){
+      for (var i = 0; i < dayta.length; i++) {
+      console.log("title:",  dayta[i].title );
+    }//end forloop
+  };//end printfunc
+    var search = function(){
+      $.ajax({
+        url: searcho,
+        dataType:'JSON',
+        success: function(data){
+          console.log("heres what I found:", data);
+        }//end success function
+      });//end AJAX
+    };//end search func
 
+    $('#button').on('click', function(){
+      //assign searchBox value to var
+      console.log('clicked');
+      var title = $('#searchBox').val();
+      var searcho = 'http://www.omdbapi.com/?s=' + title;
+      search();
 
-
-})//end jquery
+    });//end buttonclick
+});//end docReady
